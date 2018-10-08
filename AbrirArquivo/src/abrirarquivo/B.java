c738bb8b74d15ecec2893bfe62b569ed46d69388
ice <ice@lab06m04.nrc.ice.ufjf.br>
2018-10-08 20:44:12 -0300

    Projeto Petshop

---
+++ AbrirArquivo/src/abrirarquivo/B.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abrirarquivo;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author ice
+ */
+public class B {
+    public void m2() throws FileNotFoundException
+    {
+    m3();
+    }
+    public void m3() throws FileNotFoundException{
+    C c = new C();
+    c.m4();
+    
+    
+    
+    
+    }
+    
+    
+    
+}
